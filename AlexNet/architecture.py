abec6f3fc9a6ba255da030f4ed4965c12713a38d
Kangmin Choi <strongest0502@gmail.com>
2021-02-06 23:26:47 +0900

    Update architecture.py

--- AlexNet/architecture.py
+++ AlexNet/architecture.py
@@ -1,5 +1,5 @@
-import torch
-import torch.nn as nn 
+import torch.nn as nn
+import torch.nn.functional as F 
 from typing import Any
 
 class AlexNet(nn.Module):
@@ -7,28 +7,28 @@ class AlexNet(nn.Module):
   def __init__(self, num_classes: int = 1000) -> None:
     super(AlexNet, self).__init__()
     self.features = nn.Sequential(
-        # first layer(convolutional layer)
-        nn.Conv2d(in_channels=3, out_channels=96, stride=4, padding=2),
+        #first layer(convolutional layer)
+        nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, stride=4, padding=0),
         nn.ReLU(inplace=True),
-        nn.LocalResponseNorm(size=5, k=2),
+        nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
         nn.MaxPool2d(kernel_size=3, stride=2),
-        # second layer(convolutional layer)
-        nn.Conv2d(in_channels=96, out_channels=256, stride=1, padding=2),
+        #second layer(convolutional layer)
+        nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5, stride=1, padding=2),
         nn.ReLU(inplace=True),
-        nn.LocalResponseNorm(size=5, k=2),
+        nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
         nn.MaxPool2d(kernel_size=3, stride=2),
-        # third layer(convolutional layer)
-        nn.Conv2d(in_channels=256, out_channels=384, stride=1, padding=1),
+        #third layer(convolutional layer)
+        nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, stride=1, padding=1),
         nn.ReLU(inplace=True),
-        # fourth layer(convolutional layer)
-        nn.Conv2d(in_channels=384, out_channels=384, stride=1, padding=1),
+        #fourth layer(convolutional layer)
+        nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, stride=1, padding=1),
         nn.ReLU(inplace=True),
-        # fifth layer(convolutional layer)
-        nn.Conv2d(in_channels=384, out_channels=256, stride=1, padding=1),
+        #fifth layer(convolutional layer)
+        nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, stride=1, padding=1),
         nn.ReLU(inplace=True),
-        nn.MaxPool2d(kernal_size=3, stride=2)
+        nn.MaxPool2d(kernel_size=3, stride=2)
     )
-
+    
     # dense layers(include sixth, seventh, eighth FC layers)
     self.classifier = nn.Sequential(
             nn.Dropout(),
@@ -40,8 +40,10 @@ class AlexNet(nn.Module):
             nn.Linear(4096, num_classes),
         )
     
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.features(x)
-        x = torch.flatten(x, 1)
-        x = self.classifier(x)
-        return x
+  def forward(self, x: torch.Tensor) -> torch.Tensor:
+      x = self.features(x)
+      x = torch.flatten(x, 1)
+      x = self.classifier(x)  
+      return x
+
+net = AlexNet()
